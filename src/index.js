9e5d6e00467e4455fdf6be9b1aa7eb9b6bcebd7e
Daniel <danineitor.gt.rs@gmail.com>
2024-12-04 20:23:44 -0800

    Configuracion de Express

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 import express from 'express'
-
+import { engine } from 'express-handlebars';
+import morgan from 'morgan';
+import {join,dirname} from 'path'
+import { fileURLToPath } from 'url';
 // Inicializacion
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // Configuraion
 app.set('port', process.env.PORT || 3000);
+app.set("views", join(__dirname, 'views'));
+app.engine(".hbs", engine({
+    defaultLayout: 'main',
+    layoutsDir: join(app.get('views'), 'layouts'),
+    partialsDir: join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
 // Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
 
 //Rutas
+app.get('/', (req,res)=>{
+    res.json({'message':"Hola mundo"})
+});
 
 //Archivos publicos
+app.use(express.static(join(__dirname, 'public')));
 
 // Run server
 app.listen(app.get('port'), ()=>console.log('Server listening on port', app.get('port')));
