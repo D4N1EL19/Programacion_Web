052f6d297f212e3a10bf24c3f5728eac95d5874a
Daniel <danineitor.gt.rs@gmail.com>
2024-12-06 06:57:34 -0800

    Validaciones Suscripcion

--- src/public/scripts/flotante.js
+++ src/public/scripts/flotante.js
@@ -2,13 +2,28 @@ const openModal = document.getElementById('open-modal');
 const closeModal = document.getElementById('close-modal');
 const modal = document.getElementById('modal');
 const background = document.getElementById('background');
+
 const subscribeButton = document.getElementById('subscribe-btn');
-const subscriptionForm = document.getElementById('subscription-cont');
+const subscriptionCont = document.getElementById('subscription-cont');
+const closeSub = document.getElementById("closeSuscripcion");
 
 subscribeButton.addEventListener('click', () => {
-    subscriptionForm.classList.remove('hidden');
-    subscriptionForm.classList.add('flex');
-  });
+    subscriptionCont.classList.remove('hidden');
+    subscriptionCont.classList.add('flex');
+});
+
+subscriptionCont.addEventListener('click', (e)=>{
+    if (e.target === subscriptionCont) {
+        subscriptionCont.classList.add('hidden');
+        subscriptionCont.classList.remove('flex');
+    }
+});
+
+closeSub.addEventListener('click', ()=>{
+    subscriptionCont.classList.add('hidden');
+    subscriptionCont.classList.remove('flex');
+    limpiarFormulario();
+});
 
 
 openModal.addEventListener('click', () => {
@@ -28,7 +43,102 @@ modal.addEventListener('click', (e) => {
     }
 });
 
-document.querySelector('form').addEventListener('submit', async (event) => {
+function limpiarFormulario() {
+    const inputs = document.querySelectorAll('form input, form select');
+
+    inputs.forEach(input => {
+      if(input.type === "checkbox" || input.type === "radio") {
+        input.checked = false;
+      }else if(input.tagName.toLowerCase() === 'select') {
+        input.selectedIndex = 0;
+      }
+      else {
+        input.value = ''; 
+      }
+    });
+}
+
+function validarFormulario(event) {
+    const inputs = document.querySelectorAll('form input, form select');
+  
+    let esValido = true;
+    let mensajeError = '';
+    let password = '';
+    let confirmarPassword = '';
+  
+    inputs.forEach(input => {
+        esValido = true;
+        input.style.borderColor = ''; 
+
+        if (input.tagName.toLowerCase() === 'input') {
+            if (input.value.trim() === '') {
+            esValido = false;
+            mensajeError = 'Por favor, complete todos los campos.';
+            input.style.borderColor = 'red'; 
+            }
+        }
+
+        if (input.name === 'cvv' && input.value.length !== 3) {
+            esValido = false;
+            mensajeError = 'El CVV debe tener exactamente 3 dígitos.';
+            input.style.borderColor = 'red';
+        }
+    
+        if (input.name === 'numero_tarjeta' && input.value.length !== 16) {
+            esValido = false;
+            mensajeError = 'El número de tarjeta debe tener exactamente 16 dígitos.';
+            input.style.borderColor = 'red'; 
+        }
+    
+        if (input.name === 'codigo_postal' && input.value.length !== 5) {
+            esValido = false;
+            mensajeError = 'El código postal debe tener exactamente 5 dígitos.';
+            input.style.borderColor = 'red';
+        }
+    
+        if (input.name === 'password') {
+            password = input.value;
+            if (input.value.length < 8) {
+            esValido = false;
+            mensajeError = 'La contraseña debe tener al menos 8 caracteres.';
+            input.style.borderColor = 'red'; 
+            }
+        }
+    
+        if (input.name === 'confirmPassword') {
+            confirmarPassword = input.value;
+            if (confirmarPassword && confirmarPassword !== password) {
+            esValido = false;
+            mensajeError = 'Las contraseñas no coinciden.';
+            input.style.borderColor = 'red'; 
+            }
+        }
+
+        input.addEventListener('focus', () => {
+            input.style.borderColor = '';
+        });
+        
+        if (input.tagName.toLowerCase() === 'select') {
+            if (input.value.trim() === '' || input.options[0].selected) {
+                esValido = false;
+                input.style.borderColor = 'red';
+            }
+        }
+
+        input.addEventListener('focus', () => {
+            input.style.borderColor = '';
+        });
+    });
+  
+    if (!esValido) {
+      event.preventDefault();
+      alert(mensajeError);
+    }
+  }
+  
+
+
+async function validarLogin(event){
     event.preventDefault();
 
     const username = document.getElementById('username').value.trim();
@@ -55,4 +165,4 @@ document.querySelector('form').addEventListener('submit', async (event) => {
     } catch (error) {
         alert('Error al conectar con el servidor');
     }
-});
+};
